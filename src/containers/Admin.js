fb1606dbf4a2231ba3cd97f0259b6e9f056bf3f2
Cristiano Bianchi <cristiano.bianchi@gmail.com>
2017-12-30 19:35:45 +0100

    Added style to aphorism cards. Show buttons to open and delete aphorism card.

--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Header, Container, List, Button, Card, Icon } from 'semantic-ui-react';
+import { Header, Container, List, Button, Card, Icon, Dimmer, Segment, Image } from 'semantic-ui-react';
 import { invokeApig } from '../libs/awsLib';
 import Login from './Login';
 import Signup from './Signup';
@@ -13,7 +13,8 @@ export default class Admin extends Component {
     this.state = {
       isLoading: true,
       aphorisms: [],
-      lander: 'login'
+      lander: 'login',
+      dimmedIndex: 0
     };
   }
 
@@ -39,27 +40,69 @@ export default class Admin extends Component {
   }
 
   renderAphorismsList(aphorisms) {
+    const dimmed = parseInt(this.state.dimmedIndex);
     return [{}].concat(aphorisms).map(
       (aphorism, i) =>
         i !== 0
           ?
-            <Card
-              key={aphorism.aphorismId}
-              href={`/admin/aphorisms/${aphorism.aphorismId}`}
-              onClick={this.handleAphorismClick}
+            // <Card
+            //   key={aphorism.aphorismId}
+            //   href={`/admin/aphorisms/${aphorism.aphorismId}`}
+            //   onClick={this.handleAphorismClick}
+            // >
+            //   <Card.Content>
+            //     <Card.Header>
+            //       {aphorism.author}
+            //     </Card.Header>
+            //     <Card.Meta>
+            //       {"Created: " + new Date(aphorism.createdAt).toLocaleString()}
+            //     </Card.Meta>
+            //     <Card.Description className="aphorisms-content">
+            //       {aphorism.quote.trim().split("\n")[0]}
+            //     </Card.Description>
+            //   </Card.Content>
+            // </Card>
+            <Dimmer.Dimmable
+              key={i}
+              data-index={i}
+              as={Card}
+              dimmed={i === dimmed}
+              onMouseEnter={this.handleDimmerShow}
+              onMouseLeave={this.handleDimmerHide}
             >
-              <Card.Content>
-                <Card.Header>
-                  {aphorism.author}
-                </Card.Header>
-                <Card.Meta>
-                  {"Created: " + new Date(aphorism.createdAt).toLocaleString()}
-                </Card.Meta>
-                <Card.Description className="aphorisms-content">
-                  {aphorism.quote.trim().split("\n")[0]}
-                </Card.Description>
-              </Card.Content>
-            </Card>
+              <Dimmer active={i === dimmed} onClickOutside={this.handleHide}>
+                <Button
+                  primary
+                  onClick={this.handleAphorismClick}
+                  href={`/admin/aphorisms/${aphorism.aphorismId}`}
+                >
+                  Edit
+                </Button>
+                <Button
+                  href={`/admin/aphorisms/${aphorism.aphorismId}`}
+                  onClick={this.handleDelete}
+                >
+                  Delete
+                </Button>
+              </Dimmer>
+              <Card
+                key={aphorism.aphorismId}
+                href={`/admin/aphorisms/${aphorism.aphorismId}`}
+                onClick={this.handleAphorismClick}
+              >
+                <Card.Content>
+                  <Card.Header>
+                    {aphorism.author}
+                  </Card.Header>
+                  <Card.Meta>
+                    {"Created: " + new Date(aphorism.createdAt).toLocaleString()}
+                  </Card.Meta>
+                  <Card.Description className="aphorisms-content">
+                    {aphorism.quote.trim().split("\n")[0]}
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Dimmer.Dimmable>
           :
             <Card
               key="new"
@@ -81,9 +124,48 @@ export default class Admin extends Component {
     );
   }
 
+  handleDimmerShow = e => this.setState({ dimmedIndex: e.currentTarget.getAttribute('data-index') })
+  handleDimmerHide = e => this.setState({ dimmedIndex: 0 })
+
   handleAphorismClick = event => {
     event.preventDefault();
     this.props.history.push(event.currentTarget.getAttribute("href"));
+  }
+
+  handleDelete = async event => {
+    event.preventDefault();
+
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this note?"
+    );
+
+    const noteIdUrl = event.currentTarget.getAttribute("href");
+    const noteId = noteIdUrl.split('/').pop();
+    console.log(noteId, noteIdUrl);
+
+    if (!confirmed) {
+      return;
+    }
+
+    // this.setState({ isDeleting: true });
+
+    try {
+      // TODO - Delete also the attachment
+      await this.deleteAphorism(noteIdUrl);
+      let oldNotes = this.state.aphorisms;
+      const newNotes = oldNotes.filter(n => n.aphorismId !== noteId);
+      this.setState({ aphorisms: newNotes });
+    } catch (e) {
+      alert(e);
+      // this.setState({ isDeleting: false });
+    }
+  }
+
+  deleteAphorism(noteIdUrl) {
+    return invokeApig({
+      path: noteIdUrl,
+      method: "DELETE"
+    });
   }
 
   renderLanderBtn = event => {

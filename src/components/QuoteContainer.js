86c6687ebd6f3a1e8a208db77834c9efa72667a9
Cristiano Bianchi <cristiano.bianchi@gmail.com>
2018-01-13 17:18:33 +0100

    Added style to QuoteContainer

--- src/components/QuoteContainer.js
+++ src/components/QuoteContainer.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Container, Image } from 'semantic-ui-react';
+import { Container, Image, Segment } from 'semantic-ui-react';
 import config from "../config";
 import "./QuoteContainer.css";
 
+const style = {
+  height: "100vh",
+  width: "100%",
+  background: "url('" + config.urlImg + "') no-repeat center center fixed",
+  backgroundSize: "cover",
+  padding: "10em",
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  justifyContent: "center"
+};
+
 export default ({...props}) =>
-  <Container>
-    <h3>{props.aphorism.quote}</h3>
-    <p>{props.aphorism.aphorismId}</p>
-    <Image src={config.urlImg} fluid />
+  <Container style={style}>
+    <Segment className="quote-segment" raised>
+      <h3>{props.aphorism.quote}</h3>
+      <p>{props.aphorism.aphorismId}</p>
+    </Segment>
   </Container>;
